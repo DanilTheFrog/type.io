007364c73f26557a46032cb01430a02162515800
Joloy <51697931+Distrubilea8r@users.noreply.github.com>
2023-08-30 00:15:13 +0700

    add training component to training page

--- src/pages/training/ui/Page/Page.tsx
+++ src/pages/training/ui/Page/Page.tsx
@@ -1,9 +1,12 @@
+import { TrainingComponent } from "@/widgets/TypeTraining/TrainingComponent"
 
 export const TrainingPage = () => {
 
     return(
         <div>
             <h1>Тренажер</h1>
+            <TrainingComponent text="Try to type it if you have hands" />
+
         </div>
     )
 }
